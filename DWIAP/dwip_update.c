8f9277b6df3d7814bbc8c0310862117abd156367
Gaofeng <gaofengdiskonline@gmail.com>
2012-04-20 13:57:32 +0800

    注释改为中文。文件名似乎不支持中文。

--- DWIAP/dwip_update.c
+++ DWIAP/dwip_update.c
@@ -20,7 +20,7 @@ typedef struct FileHeader_st
 
 #define CHUNK_SIZE  (0x400 * 5)
 
-int GenerateUpdateFile(const u8* bin_file_path, const u8* dat_file_path, const u8* product_name, const u8* version)
+int GenerateUpdateFile(const char* bin_file_path, const char* dat_file_path, const char* product_name, const char* version)
 {
 	FileHeaderType* fht = NULL;
 	FILE* bin_file = NULL;
@@ -34,33 +34,50 @@ int GenerateUpdateFile(const u8* bin_file_path, const u8* dat_file_path, const u
 
 	if ((strlen(bin_file_path) < 4) || (memcmp(bin_file_path + strlen(bin_file_path) - 4, ".bin", 4) != 0))
 	{
-		printf("Error: Input file's extension must be .bin!\n");
+		printf("Error: 输入文件的扩展名必须为.bin。\n");
 		return 0;
 	}
 	if ((strlen(dat_file_path) < 4) || (memcmp(dat_file_path + strlen(dat_file_path) - 4, ".dat", 4) != 0))
 	{
-		printf("Error: Output file's extension must be .dat!\n");
+		printf("Error：输出文件的扩展名必须为.dat。\n");
 		return 0;
 	}
 
+    if (product_name == NULL)
+    {
+        printf("Error：产品名称为空。\n");
+        return 0;
+    }
+    if (version == NULL)
+    {
+        printf("Error：版本号为空。\n");
+        return 0;
+    }
 	bin_file = fopen(bin_file_path, "rb");
 	if (bin_file == NULL)
 	{
-		printf("Error: Can't open file %s.\n", bin_file_path);
+		printf("Error: 无法打开文件：%s.\n", bin_file_path);
 		return 0;
 	}
 	dat_file = fopen(dat_file_path, "wb");
 	if (dat_file == NULL)
 	{
-		printf("Error: Can't open or create file %s.\n", dat_file_path);
+		printf("Error: 无法打开或创建文件：%s.\n", dat_file_path);
 		fclose(bin_file);
 		return 0;
 	}
 
+    printf("正在生成升级文件...\n");
+    printf("输入文件(*.bin): %s\n", bin_file_path);
+    printf("输出文件(*.dat): %s\n", dat_file_path);
+    printf("产品名称: %s\n", product_name);
+    printf("版本号: %s\n", version);
+
+
 	fht = malloc(sizeof(FileHeaderType));
 	if (fht == NULL)
 	{
-		printf("Error: Not enough memory.\n");
+		printf("Error: 内存不足.\n");
 		return 0;
 	}
 
@@ -79,7 +96,7 @@ int GenerateUpdateFile(const u8* bin_file_path, const u8* dat_file_path, const u
 		file_buf = realloc(file_buf, file_buf_size);
 		if (file_buf == NULL)
 		{
-			printf("Error: Not enough memory.\n");
+			printf("Error: 内存不足。\n");
 			return 0;
 		}
 		read_len = fread(file_buf + file_len, 1, CHUNK_SIZE, bin_file);
@@ -102,6 +119,8 @@ int GenerateUpdateFile(const u8* bin_file_path, const u8* dat_file_path, const u
 	
 	free(file_buf);
 	free(fht);
+    printf("升级文件生成成功。\n");
+    printf("\n");
 
 	return 1;
 }
--- DWIAP/dwip_update.h
+++ DWIAP/dwip_update.h
@@ -1,4 +1,4 @@
 
 #include "type.h"
 
-int GenerateUpdateFile(const u8* bin_file_path, const u8* dat_file_path, const u8* product_name, const u8* version);
+int GenerateUpdateFile(const char* bin_file_path, const char* dat_file_path, const char* product_name, const char* version);
