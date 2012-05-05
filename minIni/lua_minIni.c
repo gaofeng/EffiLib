6bde07756118fcb53d4a54b36e9b8173620ca18e
Gaofeng <gaofengdiskonline@gmail.com>
2012-05-05 11:51:30 +0800

    添加open函数。

--- minIni/lua_minIni.c
+++ minIni/lua_minIni.c
@@ -8,6 +8,27 @@
 
 #include "minIni.h"
 
+#define FN_KEY "filename"
+
+static int l_ini_open(lua_State* L)
+{
+    const char* filename = NULL;
+    int top;
+
+    top = lua_gettop(L);
+
+    if (top != 1)
+    {
+        luaL_error(L, "Must have only one argument!");
+        return 0;
+    }
+
+    filename = luaL_checkstring(L, 1);
+    lua_setfield(L, LUA_ENVIRONINDEX, FN_KEY);
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
 static int l_ini_gets(lua_State* L)
 {
 	int top;
@@ -18,24 +39,22 @@ static int l_ini_gets(lua_State* L)
     char* buffer = NULL;
     int value_len = 0;
 
+    section = luaL_checkstring(L, 1);
+    key = luaL_checkstring(L, 2);
+
 	top = lua_gettop(L);
-	if (((top == 3) || (top == 4)) && 
-		(lua_isstring(L, -1) == 1) &&
-		(lua_isstring(L, -2) == 1))
-	{
-		section = lua_tostring(L, 1);
-		key = lua_tostring(L, 2);
-        if (top == 4)
-        {
-            default_value = lua_tostring(L, 3);
-        }
-        filename = lua_tostring(L, -1);
-	}
-	else
-	{
-		luaL_error(L, "Wrong Arguments.");
-		return 0;
-	}
+    if (top == 3)
+    {
+        default_value = luaL_checkstring(L, 3);
+    }
+
+    lua_getfield(L, LUA_ENVIRONINDEX, FN_KEY);
+    if (lua_isnil(L, -1))
+    {
+        luaL_error(L, "Must call open first!");
+        return 0;
+    }
+    filename = lua_tostring(L, -1);
 
     buffer = (char*)malloc(INI_BUFFERSIZE + 1);
     if (buffer == NULL)
@@ -46,18 +65,21 @@ static int l_ini_gets(lua_State* L)
     memset(buffer, 0x00, INI_BUFFERSIZE + 1);
     value_len = ini_gets(section, key, default_value, buffer, INI_BUFFERSIZE, filename);
 
-	lua_pushstring(L, buffer);
+	lua_pushlstring(L, buffer, value_len);
 	return 1;
 }
 
 static const luaL_reg minIni_Functions[]=
 {
+    {"open",l_ini_open},
 	{"gets",l_ini_gets},
 	{NULL,NULL}
 };
 
 int __declspec(dllexport) luaopen_minIni(lua_State* L)
 {
+    lua_newtable(L);
+    lua_replace(L, LUA_ENVIRONINDEX);
 	luaL_openlib(L,"minIni",minIni_Functions,0);
 	return 1;
 }
