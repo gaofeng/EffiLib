26131170add2257fe75b2e562a59a736ce3ee0ad
Gaofeng <gaofengdiskonline@gmail.com>
2012-08-06 17:51:18 +0800

    添加调试Console。

---
+++ Win32Generic/DBWindow.h
@@ -0,0 +1,37 @@
+﻿
+#define _DEBUG
+
+#ifndef _DBWINDOW_
+#define _DBWINDOW_
+    #include <windows.h>
+    //控制台输出时的文本颜色
+    #define WDS_T_RED    FOREGROUND_RED
+    #define WDS_T_GREEN  FOREGROUND_GREEN
+    #define WDS_T_BLUE  FOREGROUND_BLUE
+    //控制台输出时的文本背景颜色
+    #define WDS_BG_RED  BACKGROUND_RED
+    #define WDS_BG_GREEN BACKGROUND_GREEN
+    #define WDS_BG_BLUE  BACKGROUND_BLUE
+    #ifdef _DEBUG
+		#ifdef __cplusplus
+		extern "C" {
+		#endif 
+	        //设置控制台输出窗口标题
+	        BOOL DBWindowTile(LPCTSTR tile);
+	        //格式化文本输出
+	        BOOL DBWindowWrite(LPCTSTR fmt,...);
+	        //带颜色格式化文本输出
+	        //BOOL DBWindowWrite(WORD Attrs,LPCTSTR fmt,...);
+
+	    #ifdef __cplusplus
+		}
+		#endif 
+    #else
+        #define DBWindowTile
+        #define DBWindowWrite
+    #endif
+#endif
+
+
+ 
+
--- Win32Generic/generic.mak
+++ Win32Generic/generic.mak
@@ -5,10 +5,13 @@ all: generic.exe
 generic.res : generic.rc generic.h
 	rc generic.rc
 
-generic.obj : generic.c generic.h
+generic.obj : generic.c generic.h DBWindow.h DBWindow.cpp
 	cl -c -W3 -Gz -D_X86_ -DWIN32 generic.c
 
-generic.exe : generic.obj generic.res
-	link /MACHINE:I386 -subsystem:windows generic.res generic.obj kernel32.lib user32.lib gdi32.lib
+DBWindow.obj : DBWindow.cpp DBWindow.h
+	cl -c -W3 -Gz -D_X86_ -DWIN32 DBWindow.cpp
+
+generic.exe : generic.obj DBWindow.obj generic.res
+	link /MACHINE:I386 -subsystem:windows generic.res generic.obj DBWindow.obj kernel32.lib user32.lib gdi32.lib
 
 
